caccfdfb3aaae075d92f011341fba025632be4d7
Hugo Avila <99145795+bioinformagica@users.noreply.github.com>
2022-07-10 17:51:23 -0300

    Fixed: bug repeated haeders

--- workflow/scripts/extract_cassettes.py
+++ workflow/scripts/extract_cassettes.py
@@ -21,6 +21,7 @@ import sys
 from pathlib import Path
 from typing import List
 from collections import namedtuple
+import itertools
 import logging
 logger = logging.getLogger(os.path.basename(__file__).replace('.py', ''))
 from docopt import docopt
@@ -48,111 +49,6 @@ def read_defensefinder(file_path: str, separator: str, columns_to_keep: List[str
              .reset_index(drop=True) \
              .rename(columns=dict(zip(columns_to_keep, renamed_names)))
 
-def extract_cassettes_from_gbk_old(gbk_file: str, defence_genes_locus_tags: set, n_genes: int, output_path: str):
-    contig = namedtuple('Contig', 'id features defence_genes')
-    protein = namedtuple('Protein', 'locus_tag feature')
-    cassette = namedtuple('Cassette', 'locus_tags start end')
-    contigs_with_defence_genes = {}
-
-    is_overlapping = lambda a, b: b.start > a.start and b.start < a.end
-    sort_locus_tags = lambda locus_tag: int(locus_tag.split('_')[1])
-    fasta_records_to_write = []
-    output_file_name = os.path.join(output_path, 'Cassettes.faa')
-
-
-    logger.info('Retriving locus_tags from each contig ...')
-    for record in SeqIO.parse(gbk_file, 'genbank'):
-        contig_id = record.id
-        contig_features = []
-        for feature in record.features:
-            if feature.type == 'CDS':
-                try:
-                    contig_features.append(protein(feature.qualifiers['locus_tag'][0], feature))
-                except KeyError as e:
-                    pass
-
-        # check if contig has a defense system protein
-        check_locustags = tuple(map(lambda protein: protein.locus_tag, contig_features))
-        defence_genes = list(map(lambda x: x in defence_genes_locus_tags, check_locustags))
-        defence_genes = [ locus_tag for locus_tag in check_locustags if locus_tag in defence_genes_locus_tags ]
-
-        if not defence_genes:
-            continue
-
-        defence_genes = sorted(defence_genes, key=sort_locus_tags)
-
-        contigs_with_defence_genes[contig_id] = contig(contig_id, contig_features, defence_genes)
-
-
-    logger.info('There are {} contigs with defence systems.'.format(len(contigs_with_defence_genes)))
-    for contig in contigs_with_defence_genes.values():
-        cassettes = []
-        all_locus_tags = tuple(map(lambda protein: protein.locus_tag, contig.features))
-        for defence_gene in contig.defence_genes:
-            gene_index = all_locus_tags.index(defence_gene)
-            if gene_index < n_genes:
-                cassette_locus_tags = all_locus_tags[:gene_index + n_genes + 1 ]
-            else:
-                cassette_locus_tags = all_locus_tags[gene_index - n_genes : gene_index + n_genes + 1 ]
-
-            first_locus_tag, last_locus_tag = cassette_locus_tags[0], cassette_locus_tags[-1]
-            first_locus_tag_position, last_locus_tag_position = None, None
-
-            proteins = iter(contig.features)
-            protein = next(proteins, None)
-
-            while not all((first_locus_tag_position, last_locus_tag_position)) or protein:
-                if protein.locus_tag == first_locus_tag:
-                    first_locus_tag_position = protein.feature.location.start.position
-
-                if protein.locus_tag == last_locus_tag:
-                    last_locus_tag_position = protein.feature.location.end.position
-
-                protein = next(proteins, None)
-
-            cassettes.append(cassette(cassette_locus_tags, first_locus_tag_position, last_locus_tag_position))
-
-        if len(cassettes) > 1:
-            # Merging overlapping cassettes
-            cassettes.sort(key = lambda cassette: cassette.start)
-            cassettes_overlaps_merge = []
-            cassettes_overlaps_merge.append(cassettes[0])
-
-            for i in range(1, len(cassettes)):
-                pop_element = cassettes_overlaps_merge.pop()
-                if is_overlapping(pop_element, cassettes[i]):
-                    new_locus_tags = sorted(list(set([*pop_element.locus_tags, *cassettes[i].locus_tags])), key=sort_locus_tags)
-                    new_start, new_end = pop_element.start, max(pop_element.end, cassettes[i].end)
-                    new_element = cassette(new_locus_tags, new_start, new_end)
-                    cassettes_overlaps_merge.append(new_element)
-                else:
-                    cassettes_overlaps_merge.append(pop_element)
-                    cassettes_overlaps_merge.append(cassettes[i])
-        else:
-            cassettes_overlaps_merge = cassettes
-
-        logger.info('Contig {} has {} cassette(s).'.format(contig.id, len(cassettes_overlaps_merge)))
-
-        # Write cassettes
-        cassette_counter = 1
-        for i in cassettes_overlaps_merge:
-            for protein in contig.features:
-                if protein.locus_tag in i.locus_tags:
-                    try:
-                        fasta_records_to_write.append('>Contig_{}:Cassette_{}:{}\n{}\n'.format(
-                            contig.id,
-                            cassette_counter,
-                            protein.locus_tag,
-                            protein.feature.qualifiers['translation'][0]
-                        ))
-                    except KeyError as e:
-                        pass
-            cassette_counter += 1
-
-    logger.info('Writing cassettes to file {}.'.format(output_file_name))
-    with open(output_file_name, 'w') as f:
-        f.write(''.join(fasta_records_to_write))
-
 
 def parser_gbk_file(gbk_file: str) -> list:
     contig = namedtuple('Contig', 'id features')
@@ -177,19 +73,52 @@ def get_cassettes_locus_tags(all_loci: str, interest_loci: str, n_genes: int) ->
         cassettes_locus_tags.append(left_loci + right_loci)
     return cassettes_locus_tags
 
+def merge_overlapping_intervals(intervals: tuple) -> tuple:
+    stack = []
+
+    for interval in sorted(intervals, key=lambda tup: tup[0]):
+
+        if not stack:
+            stack.append(interval)
+            continue
+
+        query = stack[-1]
+
+        if interval[0] <= query[1]:
+            new_end = max(query[1], interval[1])
+            stack[-1] = (query[0], new_end)
+            continue
+
+        stack.append(interval)
+
+    return stack
+
+
 def merge_overlapping_cassettes(cassettes: tuple) -> tuple:
     get_start_end_from_cassette = lambda cassette: (cassette[0].location.start.position, cassette[-1].location.end.position)
-    is_overlapping = lambda a, b: b[1][0] > a[1][0] and b[1][0] < a[1][1]
-    cassette_ranges = list(enumerate(map(lambda x: get_start_end_from_cassette(x), cassettes)))
-    merged = []
-    for cassette_range in cassette_ranges:
+    remove_duplicated_loci = lambda merged_cassettes: sorted(set(itertools.chain.from_iterable(merged_cassettes)), key = lambda feature: feature.location.start.position)
 
+    cassettes_ranges = tuple(map(lambda cassette: get_start_end_from_cassette(cassette), cassettes))
+    merged_intervals = merge_overlapping_intervals(cassettes_ranges)
 
-    breakpoint()
+    merged_cassettes_loci = remove_duplicated_loci(cassettes)
+
+    # if there is only on interval we can just return all
+    # loci after remove duplications
+    if len(merged_intervals) == 1:
+        return [merged_cassettes_loci]
+
+    merged_cassettes = []
+    for interval in merged_intervals:
+        cassette = list(filter(lambda feature: feature.location.start.position in range(*interval), merged_cassettes_loci))
+        merged_cassettes.append(cassette)
+
+    return merged_cassettes
 
 def extract_cassettes_from_gbk(gbk_file: str, defence_genes_locus_tags: list, n_genes: int, output_path: str):
     contigs = parser_gbk_file(gbk_file)
 
+    records = []
     for contig in contigs:
         contig_locus_tags = tuple(map(lambda fet: fet.qualifiers.get('locus_tag')[0], contig.features))
         interest_loci = tuple(filter(lambda locus_tag: locus_tag in defence_genes_locus_tags, contig_locus_tags))
@@ -199,9 +128,30 @@ def extract_cassettes_from_gbk(gbk_file: str, defence_genes_locus_tags: list, n_
         cassettes_locus_tags = get_cassettes_locus_tags(all_loci=contig_locus_tags, interest_loci=interest_loci, n_genes=n_genes)
         cassettes = tuple(map(lambda cassette: tuple(filter(lambda fet: fet.qualifiers.get('locus_tag')[0] in cassette, contig.features)), cassettes_locus_tags))
         cassettes = merge_overlapping_cassettes(cassettes)
-        breakpoint()
 
+        logger.info('Contig {} have {} cassette(s).'.format(contig.id, len(cassettes)))
 
+        cassette_counter = 1
+        for cassette in cassettes:
+            for feature in cassette:
+                translation = feature.qualifiers.get('translation', [None])[0]
+                locus_tag = feature.qualifiers.get('locus_tag')[0]
+                if translation:
+                    records.append(
+                        '>Contig_{}:Cassette_{}:GOI_{}:{}\n{}\n'.format(
+                            contig.id,
+                            cassette_counter,
+                            str(locus_tag in defence_genes_locus_tags),
+                            locus_tag,
+                            translation,
+                        )
+                    )
+            cassette_counter += 1
+
+    output_file_name = os.path.join(output_path, 'Cassettes.faa')
+    logger.info('Writing cassettes to file {}.'.format(output_file_name))
+    with open(output_file_name, 'w') as f:
+        f.write(''.join(records))
 
 
 
